b01620be77b608046a5cb407474e7df70f42ec13
Chesco216 <franchescofv198@gmail.com>
2024-02-25 22:00:37 -0400

    mySolution

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
+import { Pokemon, PokemonSchema } from 'src/pokemon/entities/pokemon.entity';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
+  imports: [
+            MongooseModule.forFeature([
+              {
+                name: Pokemon.name,
+                schema: PokemonSchema,
+              }
+            ]),
+           ]
 })
 export class SeedModule {}
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,21 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
-import { readSync } from 'fs';
+import { CreatePokemonDto } from 'src/pokemon/dto/create-pokemon.dto';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class SeedService {
+
+  constructor(
+    @InjectModel( Pokemon.name )
+    private readonly pokemonModel: Model<Pokemon>
+  ) {}
 
   private readonly axios: AxiosInstance = axios;
 
     async executeSeed() {
     
       const { data: { results } } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=2');
-      results.forEach(({ name, url }) => {
+      results.forEach(async ({ name, url }) => {
         const segments = url.split('/')
         const no: number = +segments[ segments.length -2 ]
         console.log(segments)
         console.log({ name, no })
+        try {
+          // const pokemon = await this.pokemonModel.create( createPokemonDto );
+          // return pokemon;
+          const pokemon: CreatePokemonDto = {
+            name: name,
+            no: no
+          }
+          const createdPokemon = new this.pokemonModel(pokemon)
+          return createdPokemon.save();
+
+        } catch (error) {
+          throw new BadRequestException( error ); 
+        }
       });
 
       return results;
